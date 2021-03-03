ae963f30532aa4c7f5c86cf57d10341084d20f48
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-03-02 22:12:47 -0700

    add heading

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,11 @@
+#    Coding Challenge:    Odd-one-out Challenge
+#    By:                  Lyman McBride
+#    Date:                3/2/2021
+
+#    Purpose:             Made for the wednesday coding challenge at The Tech Academy,
+#                         this program prints numbers from 1-100, replacing each multiple of 3 with fizz
+#                         and each multiple of 5 with buzz. If both, with fizzbuzz.
+
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 != 0:
         print('fizz')
@@ -7,3 +15,4 @@ for i in range(1, 101):
         print('fizzbuzz')
     else:
         print(i)
+
