0cccda26c316363f9c983978018339ffadd6c20b
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-04-14 08:02:31 -0600

    finish scoreWords challenge

---
+++ scoreWords.py
@@ -0,0 +1,24 @@
+#    Coding Challenge:    Highest Scoring Words
+#    By:                  Lyman McBride
+#    Date:                4/14/2021
+
+#    Purpose:             Made for the wednesday coding challenge at The Tech Academy,
+#                         this program takes a string, and returns the word with the highest 
+#                         score according to the letters' position in the alphabet. I decided
+#                         not to control for punctuation in this one.
+
+def scoreWords(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    highWord = ""
+    highScore = 0
+    for word in string.lower().split():
+        wordScore = 0
+        for letter in word:
+            wordScore += (alphabet.index(letter)+1)
+        if wordScore > highScore: 
+            highWord = word
+            highScore = wordScore
+    print(highWord)
+    print(highScore)
+
+scoreWords("apples are waxy")
