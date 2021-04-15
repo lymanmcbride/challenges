7e1eb7ea4cf29f826a293eebc35b0fa75cd3042f
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-04-15 15:19:40 -0600

    Finish Camel Case Challenge

---
+++ camelCaseChallenge/camelCaseChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace camelCaseChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "the_stealth_warrior";
+            string[] words = str.Replace('-', ' ').Replace('_', ' ').Split(" ");
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i != 0)
+                {
+                    words[i] = myTI.ToTitleCase(words[i]);
+                }
+            }
+            string returnString = String.Join("", words);
+            Console.WriteLine(returnString);
+            Console.ReadLine();
+        }
+    }
+}
