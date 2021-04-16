e814d180a7c384c793ba6b47e0693d846dd2d113
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-04-16 11:53:44 -0600

    Finish Bit Counting Challenge

---
+++ BitCounting/BitCounting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using .net
+            int value = 10;
+            string binary = Convert.ToString(value, 2);
+            Console.WriteLine(binary);
+            int bits = binary.Count(f => (f == (char)'1'));
+            Console.WriteLine(bits);
+            Console.ReadLine();
+
+            //using strictly C#
+            Console.WriteLine(binary);
+            int bitCount = 0;
+            foreach (char num in binary)
+            {
+                if (num == (char)'1')
+                {
+                    bitCount++;
+                }
+            }
+            Console.WriteLine(bitCount);
+            Console.ReadLine();
+        }
+    }
+}
