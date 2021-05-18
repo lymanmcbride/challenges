a25576b8897a86daddaf919647796f2cf5fa22be
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-05-17 21:19:03 -0600

    Brackets Challenge First Iteration

---
+++ Brackets/Brackets/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Brackets
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string brackets = "{akls{()()zyx}";
+            Console.WriteLine(Program.TestBrackets(brackets));
+            Console.ReadLine();
+        }
+
+        public static bool TestBrackets(string brackets)
+        {
+            string completeBrackets = "{}";
+            var stack = new Stack<char>();
+            foreach (char bracket in brackets)
+            {
+                if (bracket == completeBrackets[0])
+                {
+                    stack.Push(bracket);
+                }
+                else if (bracket == completeBrackets[1])
+                {
+                    if (stack.Count == 0 || $"{stack.Pop()}{bracket}" != completeBrackets)
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
