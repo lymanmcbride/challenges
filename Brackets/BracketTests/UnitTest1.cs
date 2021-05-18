1ad2320ef3c66672a702545595d8b7b3d95f191c
Lyman McBride <68021552+lymanmcbride@users.noreply.github.com>
2021-05-17 21:53:36 -0600

    Write Unit Tests

---
+++ Brackets/BracketTests/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Brackets;
+
+namespace BracketTests
+{
+    [TestClass]
+    public class TestingBrackets
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string brackets = "{akls{()()zyx}";
+            bool F = false;
+            Assert.AreEqual(F, Program.TestBrackets(brackets));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string brackets = "{}";
+            bool T = true;
+            Assert.AreEqual(T, Program.TestBrackets(brackets));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string brackets = "}{";
+            bool F = false;
+            Assert.AreEqual(F, Program.TestBrackets(brackets));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string brackets = "{{}";
+            bool F = false;
+            Assert.AreEqual(F, Program.TestBrackets(brackets));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string brackets = "{}}";
+            bool F = false;
+            Assert.AreEqual(F, Program.TestBrackets(brackets));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string brackets = "\"\"\"";
+            bool T = true;
+            Assert.AreEqual(T, Program.TestBrackets(brackets));
+        }
+    }
+}
--- Brackets/Brackets/Program.cs
+++ Brackets/Brackets/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Brackets
 {
-    class Program
+    public class Program
     {
 
         static void Main()
