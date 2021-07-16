7e456e95e848b9cfd453a8ff9e63ec768e0709c0
Lyman McBride <lyman.mcbride@jobnimbus.com>
2021-07-16 09:52:40 -0600

    Add test project

---
+++ MoveZerosToEnd/MoveZerosTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using MoveZerosToEnd;
+
+namespace MoveZerosTests
+{
+    public class Tests
+    {
+        [Test]
+        public void StandardPass()
+        {
+            Assert.AreEqual(new int[] {1, 2, 1, 1, 3, 1, 0, 0, 0, 0}, Kata.MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1}));
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
