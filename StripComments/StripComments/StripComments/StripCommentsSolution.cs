f55105b79356b1dcceef5f12612705e16e3ddf53
Lyman McBride <lyman.mcbride@jobnimbus.com>
2021-07-26 08:28:10 -0600

    Strip Comments Challenge Initial Commit

---
+++ StripComments/StripComments/StripComments/StripCommentsSolution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StripComments
+{
+    public class StripCommentsSolution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            
+            return text;
+        }
+    }
+}
