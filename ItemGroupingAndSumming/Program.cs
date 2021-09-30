f69db8af41bd1788dc6a379f93b0d642d8d8a834
Lyman McBride <lyman.mcbride@jobnimbus.com>
2021-09-29 21:31:46 -0600

    Add Unit Tests project

--- ItemGroupingAndSumming/Program.cs
+++ ItemGroupingAndSumming/Program.cs
@@ -4,22 +4,10 @@ using System.Linq;
 
 namespace ItemGroupingAndSumming
 {
-    class Program
+    public static class Program
     {
         // Alright, I’ll post the first problem. Given a List of the following object:
-        public class Item
-        {
-            public string Category { get; }
-            public string Selector { get; }
-            public int Amount { get; }
-        
-            public Item(string category, string selector, int amount)
-            {
-                Category = category;
-                Selector = selector;
-                Amount = amount;
-            }
-        }
+
         // Write an algorithm that will return a list of “collapsed” similar items. Items are considered similar if they have the same Category and Selector .
         // Collapsed items should have their Amount summed.
         //     Example:
@@ -37,23 +25,25 @@ namespace ItemGroupingAndSumming
         // }
         static void Main(string[] args)
         {
-            List<Item> input = new List<Item>
-            {
-                new Item("c1", "s2", 25),
-                new Item("c1", "s1", 5),
-                new Item("c1", "s2", 20),
-                new Item("c1", "s2", 10),
-                new Item("c1", "s1", 15)
+            Console.WriteLine("Hello World");
+        }
 
-
-            };
-            var items = input.GroupBy(x => new { x.Category, x.Selector}).Select(grouping => new Item(grouping.Key.Category, grouping.Key.Selector, grouping.Sum(x => x.Amount))).ToList();
-
-            foreach (var item in items)
-            {
-                Console.WriteLine($"{item.Category}, {item.Selector}, {item.Amount}");
-            }
-            Console.ReadLine();
+        public static List<Item> GroupAndSum(List<Item> items)
+        {
+            return items.GroupBy(x => new { x.Category, x.Selector}).Select(grouping => new Item(grouping.Key.Category, grouping.Key.Selector, grouping.Sum(x => x.Amount))).ToList();
+        }
+    }
+    public class Item
+    {
+        public string Category { get; }
+        public string Selector { get; }
+        public int Amount { get; }
+        
+        public Item(string category, string selector, int amount)
+        {
+            Category = category;
+            Selector = selector;
+            Amount = amount;
         }
     }
 }
