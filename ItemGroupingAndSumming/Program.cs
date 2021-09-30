7dd3a0272977e1f6e3a306b41873a10b0833fa4b
Lyman McBride <lyman.mcbride@jobnimbus.com>
2021-09-30 07:43:08 -0600

    Cleanup

--- ItemGroupingAndSumming/Program.cs
+++ ItemGroupingAndSumming/Program.cs
@@ -7,24 +7,6 @@ namespace ItemGroupingAndSumming
 {
     public static class Program
     {
-        // Alright, I’ll post the first problem. Given a List of the following object:
-
-        // Write an algorithm that will return a list of “collapsed” similar items. Items are considered similar if they have the same Category and Selector .
-        // Collapsed items should have their Amount summed.
-        //     Example:
-        // List<Item> input = new List<Item>
-        // {
-        //     new Item("c1", "s1", 5),
-        //     new Item("c1", "s2", 10),
-        //     new Item("c1", "s1", 15)
-        // }
-        //
-        // List<Item> expected = new List<Item>
-        // {
-        //     new Item("c1", "s1", 20),
-        //     new Item("c1", "s2", 10),
-        // }
-
         public static List<Item> GroupAndSum(List<Item> items)
         {
             return items.GroupBy(x => new { x.Category, x.Selector}).Select(grouping => new Item(grouping.Key.Category, grouping.Key.Selector, grouping.Sum(x => x.Amount))).ToList();
