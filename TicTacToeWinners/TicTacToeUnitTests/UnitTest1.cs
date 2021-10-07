2e3f52f129675ea8d98df3c6a95792019de65eb2
Lyman McBride <lyman.mcbride@jobnimbus.com>
2021-10-07 13:57:17 -0600

    Initial Setup

---
+++ TicTacToeWinners/TicTacToeUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacToeUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TicTacToeWinners/TicTacToeWinners/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeWinners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
