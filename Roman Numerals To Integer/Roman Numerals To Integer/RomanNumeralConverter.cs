05d4c0227db285983fd6a993f4ef1ae810443a80
Lyman McBride <lyman.mcbride@jobnimbus.com>
2022-06-09 10:02:17 -0600

    Finish Convert Roman Numerals to Integers Challenge

--- Roman Numerals To Integer/Roman Numerals To Integer/RomanNumeralConverter.cs
+++ Roman Numerals To Integer/Roman Numerals To Integer/RomanNumeralConverter.cs
@@ -1,22 +1,66 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Roman_Numerals_To_Integer
 {
     public class RomanNumeralConverter
     {
-        public int ConvertToInteger(string romanNumeral) 
+        public int ConvertToInteger(string romanNumeral)
         {
-            Dictionary<string, int> romanNumeralMappings = new Dictionary<string, int>()
+            int result = 0;
+
+            for (int i = 0; i < romanNumeral.Length; i++)
             {
-                {"I",1},
-                {"V", 5},
-                {"X", 10},
-                {"L", 50},
-                {"C", 100},
-                {"D", 500},
-                {"M", 1000},
+                char currentLetter = romanNumeral[i];
+                if (i != romanNumeral.Length - 1 && CheckForSubtractionCase(currentLetter, romanNumeral[i + 1]))
+                {
+                    result += RomanNumeralMappings.RomanNumeralToInteger[romanNumeral[i + 1]] -
+                              RomanNumeralMappings.RomanNumeralToInteger[romanNumeral[i]];
+                    i++;
+                }
+                else
+                {
+                    result += RomanNumeralMappings.RomanNumeralToInteger[currentLetter];
+                }
+            }
+            
+            return result;
+        }
+
+        private bool CheckForSubtractionCase(char currentCharacter, char nextCharacter)
+        {
+            bool subtractionCaseExists = false;
+            if (RomanNumeralMappings.SubtractingNumerals.ContainsKey(currentCharacter))
+            {
+                if (RomanNumeralMappings.SubtractingNumerals[currentCharacter].Contains(nextCharacter))
+                {
+                    subtractionCaseExists = true;
+                }
+            }
+
+            return subtractionCaseExists;
+        }
+
+        private static class RomanNumeralMappings
+        {
+            public static readonly Dictionary<char, int> RomanNumeralToInteger = new Dictionary<char, int>
+            {
+                {'I',1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
             };
-            return romanNumeralMappings[romanNumeral];
+
+            public static readonly Dictionary<char, char[]> SubtractingNumerals = new Dictionary<char, char[]>
+            {
+                { 'I', new[] { 'V', 'X' } },
+                { 'X', new[] { 'L', 'C' } },
+                { 'C', new[] { 'D', 'M' } },
+            };
         }
     }
 }
---
+++ Roman Numerals To Integer/Roman Numerals To Integer/RomanNumeralConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Roman_Numerals_To_Integer
+{
+    [TestFixture]
+    public class RomanNumeralConverterTests
+    {
+        private readonly RomanNumeralConverter _romanNumeralConverter;
+        public RomanNumeralConverterTests()
+        {
+            _romanNumeralConverter = new RomanNumeralConverter();
+        }
+        [Test]
+        public void ShouldWorkIfTestCaseIsOnlyAddition()
+        {
+            const string romanNumeral = "XXVII";
+            const int expectedInteger = 27;
+            Assert.True(_romanNumeralConverter.ConvertToInteger(romanNumeral) == expectedInteger);
+        }
+        [Test]
+        public void ShouldWorkIfTestCaseIsSimpleSubtraction()
+        {
+            const string romanNumeral = "IV";
+            const int expectedInteger = 4;
+            Assert.True(_romanNumeralConverter.ConvertToInteger(romanNumeral) == expectedInteger);
+        }
+        
+        [Test]
+        public void ShouldWorkIfTestCaseIsAdditionAndOneSubtraction()
+        {
+            const string romanNumeral = "XIV";
+            const int expectedInteger = 14;
+            Assert.True(_romanNumeralConverter.ConvertToInteger(romanNumeral) == expectedInteger);
+        }
+        
+                
+        [Test]
+        public void ShouldWorkIfTestCaseHasComplicatedAdditionAndSubtraction()
+        {
+            const string romanNumeral = "MCMXCIV";
+            const int expectedInteger = 1994;
+            Assert.True(_romanNumeralConverter.ConvertToInteger(romanNumeral) == expectedInteger);
+        }
+    }
+}
